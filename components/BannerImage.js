aea1bcdb7685a520b090367b7f967400d81f196f
IvanPJH05 <ivanphang05@gmail.com>
2026-06-21 04:58:45 +0800

    Remove unused banner image section

--- components/BannerImage.js
+++
@@ -1,9 +0,0 @@
-import { assetUrl } from "@/lib/assets";
-
-export default function BannerImage() {
-  return (
-    <section className="banner-image-section" aria-label="Meaningful Plushies feature image">
-      <img src={assetUrl("/assets/home-2.png")} alt="" />
-    </section>
-  );
-}
